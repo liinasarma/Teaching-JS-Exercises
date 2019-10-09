53004cba74e3721c484b82fc9314bbd8df5b9640
liinasarma <lina.sarma@gmail.com>
2019-10-09 17:41:54 +0300

    pievienots index.html un sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let canvas;
+
+function setup(){
+canvas = createCanvas(windowWidth,windowHeight);
+canvas.position(0,0);
+canvas.style('z-index', '-1');
+
+
+background(248, 250, 137);
+}
+
+function draw(){
+
+
+point(mouseX, mouseY);
+//
+// for(let i = 0; i<windowWidth; i+=5){
+//
+// strokeWeight(0.5);
+// stroke(153);
+// line(0, i, 500, i);
+//
+//
+// }
+
+
+
+
+}
